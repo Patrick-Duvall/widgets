e04ca4201b9cc3cb2a072779c911b1bf82b1c298
Patrick Duvall <duvallp208@gmail.com>
2021-01-28 10:23:15 -0700

    add debouncing for translate

--- src/components/Convert.jsx
+++ src/components/Convert.jsx
@@ -3,14 +3,25 @@ import axios from 'axios'
 
 const Convert = ({language, text}) => {
   const [translated, setTranslated] = useState('')
-  useEffect(() => {
+  const[debouncedText, setDebouncedText] = useState(text)
 
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedText(text)
+    }, 500);
+
+    return() => {
+      clearTimeout(timerId);
+    };
+  }, [text])
+
+  useEffect(() => {
     const doTranslation = async () => {
       const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2',
         {},
         {
           params: {
-            q: text,
+            q: debouncedText,
             target: language.value,
             key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms - IwDlM' //OK to expose
           }
@@ -21,7 +32,7 @@ const Convert = ({language, text}) => {
     }
 
     doTranslation();
-  }, [language, text])
+  }, [language,debouncedText])
   return (
     <div>
       <h1 className='ui header'>{translated}</h1>
