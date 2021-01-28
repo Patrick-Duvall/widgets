a85329ea9f446588fb7615514dd14c9da33845e7
Patrick Duvall <duvallp208@gmail.com>
2021-01-28 13:47:38 -0700

    Add link, make page not refresh on nav

--- src/components/Route.jsx
+++ src/components/Route.jsx
@@ -1,6 +1,20 @@
+import { useEffect, useState } from 'react'
+
 const Route = ({path, children}) => {
-  console.log(window.location.pathname);
-  console.log(path);
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+
+    window.addEventListener('popstate', onLocationChange)
+
+    return () => {
+      window.removeEventListener('popstate', onLocationChange)
+    }
+  }, []);
+
   return window.location.pathname === path
     ? children
     : null;
